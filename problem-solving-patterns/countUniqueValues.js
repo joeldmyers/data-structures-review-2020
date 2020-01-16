2ea634b16a34861a6b71f5eab785210cfb86d88a
Joel Myers <joeldmyers@gmail.com>
2020-01-15 22:08:01 -0500

    using counts as a different approach

--- problem-solving-patterns/countUniqueValues.js
+++ problem-solving-patterns/countUniqueValues.js
@@ -1,9 +1,27 @@
+// set approach.
+// const countUniqueValues = array => {
+//   const set = new Set();
+//   array.forEach(item => {
+//     set.add(item);
+//   });
+//   return set.size;
+// };
+
+// moving pointers approach.
+
 const countUniqueValues = array => {
-  const set = new Set();
+  // note, array must be sorted.
+
+  let uniqueCount = 0;
+  let previousItem;
+
   array.forEach(item => {
-    set.add(item);
-    return;
+    if (item !== previousItem) {
+      uniqueCount++;
+    }
+    previousItem = item;
   });
-  return set.size;
+  return uniqueCount;
 };
+
 module.exports = countUniqueValues;
